9d6ee7b885842ea4f1eb5674eb77e4fadb2a42f3
José <jruedat@unal.edu.co>
2022-09-10 17:29:10 -0500

    perro hp

--- TablesCreation.sql
+++ TablesCreation.sql
@@ -3,3 +3,18 @@ CREATE TABLE cooperativa(
     nombre VARCHAR2(30) NOT NULL UNIQUE,
     c_acumulado NUMBER(8)
 );
+
+CREATE TABLE socio(
+    idsocio NUMBER(8) PRIMARY KEY,
+    nombre VARCHAR2(30) NOT NULL,
+    s_acumulado NUMBER(11,3) CHECK(s_acumulado >= 0)
+);
+
+CREATE TABLE coopexsocio(
+    socio NUMBER(8) REFERENCES socio,
+    coope NUMBER(11, 3) REFERENCES cooperativa,
+    PRIMARY KEY(socio, coope),
+    sc_acumulado NUMBER(8)
+);
+
+COMMIT;
