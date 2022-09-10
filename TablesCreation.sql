a7af1165b0495d486e307880ec98b48de24f94a2
José <jruedat@unal.edu.co>
2022-09-10 17:38:13 -0500

    as

--- TablesCreation.sql
+++ TablesCreation.sql
@@ -12,9 +12,9 @@ CREATE TABLE socio(
 
 CREATE TABLE coopexsocio(
     socio NUMBER(8) REFERENCES socio,
-    coope NUMBER(11, 3) REFERENCES cooperativa,
+    coope NUMBER(8) REFERENCES cooperativa,
     PRIMARY KEY(socio, coope),
-    sc_acumulado NUMBER(8)
+    sc_acumulado NUMBER(11,3)
 );
 
 COMMIT;
