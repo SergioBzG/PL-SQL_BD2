d7838ef9d2e0d3fd281e67794c78874f94fcefa6
José <jruedat@unal.edu.co>
2022-09-10 21:10:50 -0500

    Creation of trigger delete cooperative
    this was a painfull job

---
+++ triggerBorradoCooperativa.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE TRIGGER borrado_cooperativa
+BEFORE DELETE ON cooperativa
+FOR EACH ROW
+DECLARE
+    CURSOR c_coopxsocio(cooperativa_borrada NUMBER) IS SELECT socio, sc_acumulado FROM coopexsocio WHERE coope = cooperativa_borrada;
+    cooperativa_borrada cooperativa.codigo%TYPE;
+BEGIN
+    cooperativa_borrada:= :OLD.codigo;
+    FOR i IN c_coopxsocio(cooperativa_borrada) LOOP
+        UPDATE socio SET s_acumulado = s_acumulado - i.sc_acumulado WHERE idsocio = i.socio;
+    END LOOP;
+    DELETE FROM coopexsocio WHERE coope = cooperativa_borrada;
+END;
+/
+COMMIT;
