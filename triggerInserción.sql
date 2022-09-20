2b8ff438d50bbda323b5bbcccbf43c2ad0e8d0e4
José <jruedat@unal.edu.co>
2022-09-20 11:08:14 -0500

    Update triggerInserción.sql

--- triggerInserción.sql
+++ triggerInserción.sql
@@ -3,7 +3,6 @@ BEFORE INSERT ON cooperativa
 FOR EACH ROW
 DECLARE
 BEGIN
-    UPDATE cooperativa SET cooperativa.c_acumulado = 0;
     :NEW.c_acumulado := 0;
 END;
 /
@@ -13,7 +12,6 @@ BEFORE INSERT ON socio
 FOR EACH ROW
 DECLARE
 BEGIN
-    UPDATE socio SET socio.s_acumulado = 0;
     :NEW.s_acumulado := 0;
 END;
 /
@@ -23,7 +21,6 @@ BEFORE INSERT ON coopexsocio
 FOR EACH ROW
 DECLARE
 BEGIN
-    UPDATE coopexsocio SET coopexsocio.sc_acumulado = 0;
     :NEW.sc_acumulado := 0;
 END;
 /
